60f5183658b3793467eec98c97709bc5bfdf676a
shubhangidourby <76735411+shubhangidourby@users.noreply.github.com>
2024-02-12 13:35:39 +0530

    Update Header.js

--- src/componenet/Header.js
+++ src/componenet/Header.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../css/Header.css';
 
+const Header = () => {
+  return (
+    <header className="App__header">
+      <h1 className="App__title">PUNK BEER</h1>
+      <nav className="App__nav">
+        <ul className="menu-list">
+          <li className="menu-list__item">
+            <NavLink className="menu-list__link" exact to="/">Home</NavLink>
+          </li>
+          <li className="menu-list__item">
+            <NavLink className="menu-list__link" to="/favourites">Favourites</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
