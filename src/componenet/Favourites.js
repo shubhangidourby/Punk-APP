73a25acfdcf04330a0d79eaf4f7e955cd3c45f07
shubhangidourby <76735411+shubhangidourby@users.noreply.github.com>
2024-02-12 18:53:21 +0530

    Update Favourites.js

--- src/componenet/Favourites.js
+++ src/componenet/Favourites.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import RenderBeerList from './RenderBeerList';
+
 import '../css/Beers.css';
 
-const Favourites = ({ beers, toggleFavourite }) => {
+const Favourites = ({ beers, toggleFavourite}) => { 
   const beerList = beers.length ? (
-    <RenderBeerList beers={beers} toggleFavourite={toggleFavourite} />
+    <>
+     
+      <RenderBeerList beers={beers} toggleFavourite={toggleFavourite} />
+    </>
   ) : (
-      <p className="info">Add your Favourites Beers...!</p>
-    );
+    <p className="info">Add your Favourites Beers...!</p>
+  );
 
   return (
     <div className="container">
