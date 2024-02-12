7477f55f619823a6c36d51ec973884211eed6a8a
shubhangidourby <76735411+shubhangidourby@users.noreply.github.com>
2024-02-12 17:53:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,118 +1,112 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Beers from './components/Beers';
 import Favourites from './components/Favourites';
 
-class App extends Component {
-  state = {
-    items: [],
-    favourites: localStorage.getItem('Favourites') ? JSON.parse(localStorage.getItem('Favourites')) : [],
-    isLoaded: false,
-    error: null
-  }
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [favourites, setFavourites] = useState(
+    localStorage.getItem('Favourites')
+      ? JSON.parse(localStorage.getItem('Favourites'))
+      : []
+  );
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const observer = useRef();
 
-  toggleFavourite = async (e, id) => {
-    const beer = this.state.items.find(item => item.id === id);
-    let favourites;
+  const lastBeerRef = useCallback(node => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPage(prevPage => prevPage + 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+  }, [loading, hasMore]);
+
+  const toggleFavourite = async (e, id) => {
+    const beer = items.find(item => item.id === id);
+    let updatedFavourites;
     if (!beer.isFavourite) {
       e.target.className = 'fas fa-star';
       e.target.setAttribute('title', 'Remove from Favourites');
-      favourites = [...this.state.favourites, beer];
+      updatedFavourites = [...favourites, beer];
     } else {
       e.target.className = 'far fa-star';
       e.target.setAttribute('title', 'Add to Favourites');
-      favourites = this.state.favourites.filter(favourite => favourite.id !== id);
+      updatedFavourites = favourites.filter(
+        favourite => favourite.id !== id
+      );
     }
     beer.isFavourite = !beer.isFavourite;
-    await this.setState({ favourites });
-    localStorage.setItem('Favourites', JSON.stringify(this.state.favourites));
-  }
+    await setFavourites(updatedFavourites);
+    localStorage.setItem('Favourites', JSON.stringify(updatedFavourites));
+  };
 
-  checkIfFavourite = (id) => {
-    const isFavourite = this.state.favourites.find(favourite => favourite.id === id);
-    if (isFavourite) return true;
-    return false;
-  }
+  const checkIfFavourite = id => {
+    const isFavourite = favourites.find(favourite => favourite.id === id);
+    return !!isFavourite;
+  };
 
-  searchBar = (beer) => {
-    const items = [];
-    fetch(`https://api.punkapi.com/v2/beers?beer_name=${beer}&page=1&per_page=80`)
-      .then(res => {
-        if (res.ok) {
-          return res.json();
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=20`);
+        const data = await response.json();
+        if (data.length === 0) {
+          setHasMore(false);
         } else {
-          throw new Error(`The connection ended with status ${res.status}${res.statusText ? ': ' + res.statusText : ''}`);
+          data.forEach(item => {
+            const beer = {
+              id: item.id,
+              name: item.name,
+              tagline: item.tagline,
+              firstBrewed: item.first_brewed,
+              desc: item.description,
+              imageURL: item.image_url,
+              isFavourite: checkIfFavourite(item.id),
+            };
+            setItems(prevItems => [...prevItems, beer]);
+          });
         }
-      })
-      .then(json => {
-        if (!json.length) return alert('We\'re sorry, we could not find such a beer :(');
-        json.forEach(item => {
-          const beer = {
-            id: item.id,
-            name: item.name,
-            tagline: item.tagline,
-            firstBrewed: item.first_brewed,
-            desc: item.description,
-            imageURL: item.image_url,
-            isFavourite: this.checkIfFavourite(item.id)
-          };
-          items.push(beer);
-        });
-        this.setState({ items });
-      })
-      .catch(error => {
-        this.setState({ error });
-        alert(error);
-      });
-  }
+      } catch (error) {
+        console.error('Error fetching beers:', error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [page]);
 
-  componentDidMount() {
-    const items = [];
-    fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')
-      .then(res => {
-        if (res.ok) {
-          this.setState({ isLoaded: true });
-          return res.json();
-        } else {
-          throw new Error(`The connection ended with status ${res.status}${res.statusText ? ': ' + res.statusText : ''}`);
-        }
-      })
-      .then(json => {
-        json.forEach(item => {
-          const beer = {
-            id: item.id,
-            name: item.name,
-            tagline: item.tagline,
-            firstBrewed: item.first_brewed,
-            desc: item.description,
-            imageURL: item.image_url,
-            isFavourite: this.checkIfFavourite(item.id)
-          };
-          items.push(beer);
-        });
-        this.setState({ items });
-      })
-      .catch(error => {
-        this.setState({ isLoaded: true, error });
-        alert(error);
-      });
-  }
-
-  render() {
-    const { items, favourites } = this.state;
-    return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path="/" render={() => <Beers beers={items} toggleFavourite={this.toggleFavourite} searchBar={this.searchBar} />} />
-            <Route path="/favourites" render={() => <Favourites beers={favourites} toggleFavourite={this.toggleFavourite} />} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Beers
+                beers={items}
+                toggleFavourite={toggleFavourite}
+                lastBeerRef={lastBeerRef}
+              />
+            )}
+          />
+          <Route
+            path="/favourites"
+            render={() => (
+              <Favourites beers={favourites} toggleFavourite={toggleFavourite} />
+            )}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
